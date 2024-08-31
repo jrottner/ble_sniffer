74051f60e49c5c2f201ad50567b5d0bdc537caa0
Joe Rottner <joerottner@Joes-MacBook-Air.local>
2024-08-31 10:23:04 -0400

    First initial push of bluetooth app in Xcode

---
+++ BLE Scanner/BLE Scanner/BLE_ScannerApp.swift
@@ -0,0 +1,18 @@
+//
+//  BLE_ScannerApp.swift
+//  BLE Scanner
+//
+//  Created by Joe Rottner on 8/31/24.
+//
+
+import SwiftUI
+import CoreBluetooth
+
+@main
+struct BLE_ScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BLE Scanner/BLE Scanner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BLE Scanner
+//
+//  Created by Joe Rottner on 8/31/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
